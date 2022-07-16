15c6ad959990afb97eef0b54e7c2f0fcae4cfa68
markPranav <pranavsri2001@gmail.com>
2022-07-16 11:07:50 +0530

    Update scrapers.py

--- Backend/backend/price_tracker/scrapers.py
+++ Backend/backend/price_tracker/scrapers.py
@@ -27,6 +27,7 @@ def genericScrapper(url):
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    print(driver)
     page = driver.get(url)
     sleep(5)
     print(page.status_code)
